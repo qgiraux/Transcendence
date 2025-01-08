47d6f01c4013cb8381be9e072ee44dd9bec56570
Nicolas Rea <nrea@paul-f4Br5s3.clusters.42paris.fr>
2025-01-08 15:15:37 +0100

    Avatar View remove duplicate import

--- avatar/avatarservice/avatar/views.py
+++ avatar/avatarservice/avatar/views.py
@@ -6,7 +6,6 @@ from django.conf import settings
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.response import Response
 from .serializers import  AvatarListSerializer
 from rest_framework import status
 from .models import Avatar
