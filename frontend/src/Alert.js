92d3b3ea6d4d09a39484d0cc89237d4b6a8313d7
Nicolas Rea <nrea@paul-f4Br6s4.clusters.42paris.fr>
2025-01-09 17:38:23 +0100

    Alert.js fix path for TRequest import

--- frontend/src/Alert.js
+++ frontend/src/Alert.js
@@ -1,5 +1,4 @@
-import TRequest from "../TRequest.js";
-import Router from "../Router.js";
+import TRequest from "./TRequest.js";
 
 class Alert {
   static errorMessage(title, message) {
@@ -34,12 +33,12 @@ class Alert {
 	</div></div></div>
 	`;
   }
-  
+
   static inviteMessage(title, message, link) {
-	const alertContainer = document.querySelector("#alert-container");
-  
-	// Generate the alert HTML
-	const alertHTML = `
+    const alertContainer = document.querySelector("#alert-container");
+
+    // Generate the alert HTML
+    const alertHTML = `
 	  <div class="row">
 		<div class="col-6 mx-auto">
 		  <div class="alert alert-primary alert-dismissible fade show" role="alert">
@@ -50,33 +49,39 @@ class Alert {
 		  </div>
 		</div>
 	  </div>`;
-  
-	// Append the alert to the container
-	alertContainer.innerHTML += alertHTML;
-  
-	// Query the new element after appending to the DOM
-	setTimeout(() => {
-	  const inviteLink = alertContainer.querySelector(".accept-invite");
-	  if (inviteLink) {
-		inviteLink.addEventListener("click", async (event) => {
-		  event.preventDefault();
-		  const form = { "name": link }; // Correct JSON structure
-		  try {
-			const ret = await TRequest.request("POST", "/api/tournament/join/", form);
-			console.log(ret);
-			this.clearAlerts();
-			Alert.successMessage("Success", `Invite accepted successfully for tournament: ${link}`);
-		  } catch (error) {
-			Alert.errorMessage("Error", "Failed to accept the invite.");
-		  }
-		});
-	  } else {
-		console.error("Invite link not found in the DOM.");
-	  }
-	}, 0); // Allow the DOM to update
+
+    // Append the alert to the container
+    alertContainer.innerHTML += alertHTML;
+
+    // Query the new element after appending to the DOM
+    setTimeout(() => {
+      const inviteLink = alertContainer.querySelector(".accept-invite");
+      if (inviteLink) {
+        inviteLink.addEventListener("click", async (event) => {
+          event.preventDefault();
+          const form = { name: link }; // Correct JSON structure
+          try {
+            const ret = await TRequest.request(
+              "POST",
+              "/api/tournament/join/",
+              form
+            );
+            console.log(ret);
+            this.clearAlerts();
+            Alert.successMessage(
+              "Success",
+              `Invite accepted successfully for tournament: ${link}`
+            );
+          } catch (error) {
+            Alert.errorMessage("Error", "Failed to accept the invite.");
+          }
+        });
+      } else {
+        console.error("Invite link not found in the DOM.");
+      }
+    }, 0); // Allow the DOM to update
   }
-  
-  
+
   static clearAlerts() {
     document.querySelector("#alert-container").innerHTML = "";
   }
