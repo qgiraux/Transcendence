4431dd00aa66a0a57b542ea250f01ecd29a6200d
Quentin Giraux <qgiraux@paul-f4Br6s8.clusters.42paris.fr>
2025-01-23 13:59:12 +0100

    tournament ok, need ready command

--- tournament/tournament/tournamentApp/tournament.py
+++ tournament/tournament/tournamentApp/tournament.py
@@ -9,6 +9,8 @@ import logging
 import uuid
 import asyncio
 from .models import Tournament
+from asgiref.sync import async_to_sync
+import random
 
 redis_client = redis.StrictRedis(host='redis', port=6379, db=0)
 
@@ -19,46 +21,54 @@ def Tournament_operation(tournament):
     logger.error("trying to start tournament...")
     try:
         lineup = tournament.player_list
+        random.shuffle(lineup)
         exp_size = tournament.tournament_size
         size = len(lineup)
         if size not in [2 ** i for i in range(1, 3)] or size != exp_size:
             logger.error(f"Invalid tournament size: {size} or exp_size: {exp_size}")
             return
         logger.error("starting tournament...")
-        winner = organize_tournament(lineup)
+        tournament.status = 1
+        tournament.save()
+        winner = organize_tournament(lineup, tournament)
         logger.error(f"[Tournament_operation] Winner: {winner}")
-        tournament.delete()
+
         return 
     except Exception as e:
         return e
 
 
-def organize_tournament(lineup):
+def organize_tournament(lineup, tournament):
+
+    tournament.rounds[len(lineup)] = lineup
+    tournament.save()
     if len(lineup) == 1:
+        tournament.status = 2
+        tournament.save()
         logger.error(f"[organize_tournament] Winner: {lineup[0]}")
         return lineup[0]  # Winner
     next_lineup = []
     logger.error(f"lineup: {lineup}")
     # Parallel execution of matches
     with ThreadPoolExecutor() as executor:
-        gamename = str(uuid.uuid4())
+        # gamename = str(uuid.uuid4())
         futures = [
-            executor.submit(asyncio.run, match(lineup[i], lineup[i + 1], f"{gamename}{i}"))
+            executor.submit(asyncio.run, match(lineup[i], lineup[i + 1], str(uuid.uuid4())))
             for i in range(0, len(lineup), 2)
         ]
         for future in futures:
             next_lineup.append(future.result())
 
-    return organize_tournament(next_lineup)
+    return organize_tournament(next_lineup, tournament)
 
-from asgiref.sync import async_to_sync
+
 
 async def match(player1, player2, gamename):
     logger.error(f"Match {gamename} between {player1} and {player2}")
     channel_layer = get_channel_layer()
 
     # Simulate a channel name for this task
-    channel_name = f"channel_{gamename}_{player1}_{player2}"
+    channel_name = f"channel_{gamename}_{player1}_{player2}".replace("-", "_")
 
     # Add the simulated channel to the group
     await channel_layer.group_add(
@@ -70,7 +80,7 @@ async def match(player1, player2, gamename):
     notification = {
         'type': 'game_message',
         'group': f'user_{player1}',
-        'message': gamename,
+        'message': f"'{gamename}'",
         'sender': "0",
     }
     try:
@@ -91,6 +101,7 @@ async def match(player1, player2, gamename):
     while True:
         logger.error(f"Waiting for message from: game_{gamename}")
         message = await channel_layer.receive(channel_name)  # Use `receive` for the specific channel
+        logger.error(f"Received message: {message}")
 
         # Check for game over condition
         if message.get("type") == "game_over":
