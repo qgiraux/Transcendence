61564221b08be2b9fb6bb44b13f2e8550e3a664e
Jeremy Perez <jerperez@made-f0Br8s17.clusters.42paris.fr>
2025-02-13 13:25:37 +0100

    sh restored

--- cli-test.sh
+++ cli-test.sh
@@ -1,67 +1,127 @@
-#
-make down
-docker volume rm $(docker volume ls)
-make
-#
+PASSWORD=Password123!
+LINE_BREAK=_____________________________________________
+user_list=""
+player_num=0
+tournament_name=""
 
-_userA="jerperez"
-_userB="nritalo"
-_passwordOK=Password123!
-_wait_time_gnome=30
+make_server () {
+    make down
+    docker volume rm $(docker volume ls)
+    make
+    sleep 5
+}
 
-#
-cd cli/
-
-#TEST VERSION
-node pong-cli --version
-
-#wait for working server
-sleep 5
-
-#TEST SIGNUP A
-_login=$_userA
-_password=$_passwordOK
+register_user () {
+    _login=$1
+    _password=$PASSWORD
 node pong-cli signup "--login=$_login" "--password=$_password" <<EOF
 $_password
 EOF
+}
 
-#TEST SIGNUP B
-_login=$_userB
-_password=$_passwordOK
-node pong-cli signup "--login=$_login" "--password=$_password" <<EOF
-$_password
-EOF
+join_tournament () {
+    _login=$1
+    _password=$PASSWORD
+    gnome-terminal -- bash -c "node pong-cli test --login=$_login --password=$_password --tournament=$tournament_name"
+}
 
-#TEST CHAT
-_login=$_userA
-_password=$_passwordOK
-gnome-terminal -- bash -c "sleep $_wait_time_gnome | node pong-cli chat --login=$_login --password=$_password <<EOF
-coucou from $_login
-!exit
-EOF
-"
+create_tournament () {
+    _login=$1
+    _password=$PASSWORD
+    _players=$player_num
+    gnome-terminal -- bash -c "node pong-cli test --login=$_login --password=$_password --tournament=$tournament_name --create --players=$_players"
+}
 
-_login=$_userB
-_password=$_passwordOK
-gnome-terminal -- bash -c "sleep $_wait_time_gnome | node pong-cli chat --login=$_login --password=$_password <<EOF
-coucou from $_login
-!exit
-EOF
-"
+#
 
-sleep 1
+get_first_user () {
+    get_user 0
+}
 
-#TEST 1v1
-_create_dt=0.5
-_join_dt=0.5
+get_other_users () {
+    echo -e $user_list | awk '{if (1 != NR) {print $0}}'
+}
 
-_login=$_userA
-_password=$_passwordOK
-_tournament=patate
-gnome-terminal -- bash -c "(sleep $_join_dt && echo ' ' && sleep 10) | node pong-cli game --login=$_login --password=$_password --width=30 --height=20 --create --tournament=$_tournament"
+echo_step () {
+    echo $LINE_BREAK
+    echo $1
+    echo $LINE_BREAK
+}
 
-_login=$_userB
-_password=$_passwordOK
-_tournament=patate
-gnome-terminal -- bash -c "(sleep $_create_dt && sleep $_join_dt && echo ' ' && sleep 10) | node pong-cli game --login=$_login --width=30 --height=20 --password=$_password --tournament=$_tournament"
+hex_to_username () {
+    echo $1 | tr 0123456789 abcdefghij #TODO: maybe crop to last 15 chars
+}
 
+dec_to_hex () {
+    echo "obase=16; $1" | bc
+}
+
+get_unique_name() {
+    time_unique=$(dec_to_hex "$(date +%s)")
+    index_unique=$(dec_to_hex "$1")
+    hex_to_username "$time_unique$index_unique"
+}
+
+add_user () {
+    user=$1
+    user_list+="$user\n"
+}
+
+get_user () {
+    index=$1
+    echo -e $user_list | awk '{if (NR - 1 == '$index') {print $0; exit}}'
+}
+
+add_unique_users () {
+    to_add=$1
+    for i in $(seq 1 $to_add);
+    do
+        add_user "u$(get_unique_name $i)"
+    done
+}
+
+get_unique_name_once () {
+    echo "t$(get_unique_name 0)"
+}
+
+#
+register_users () {
+    echo_step "USERS SIGNUP"
+    for _user in $(echo -e $user_list);
+    do
+        echo "$_user registers"
+        register_user $_user
+    done
+    echo $LINE_BREAK
+}
+
+play_tournament () {
+    echo_step "TOURNAMENT"
+    _user=$(get_first_user)
+    echo "$_user creates $tournament_name"
+    create_tournament $(get_first_user)
+    sleep 1 #
+    for _user in $(get_other_users);
+    do
+        sleep 1 #
+        echo "$_user joins $tournament_name"
+        join_tournament $_user
+    done
+    echo $LINE_BREAK
+}
+
+##############################################################
+
+player_num=2
+tournament_name=$(get_unique_name_once)
+echo $tournament_name
+
+# #
+make_server
+
+cd cli
+
+add_unique_users $player_num
+
+register_users
+play_tournament
