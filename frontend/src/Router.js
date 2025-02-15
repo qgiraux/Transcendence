1442e83e86d613718edfe70864a0c82576418dcf
Nicolas Rea <nrea@made-f0Br7s15.clusters.42paris.fr>
2025-02-15 13:03:56 +0100

    Router.js add '=' ':' to the athorized charcter set in Router.reroute()
    In case of 2fa login, the token special charcters triggered an error.

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -124,7 +124,7 @@ class Router {
 
   //AV = I added the hyphen because this rule blocked the rerouting to /create-tournaments in case of a language change
   static reroute(uri) {
-    const regex = /^\/[a-zA-Z0-9\/-]*$/;
+    const regex = /^\/[a-zA-Z0-9\/-\=\:]*$/;
     if (!regex.test(uri)) throw new Error("Router::reroute : invalid route");
     const event = new CustomEvent("redirect", {
       detail: {
