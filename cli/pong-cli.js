798639e7bd34ed09136b5766e8b9cef268f99b5a
Jeremy Perez <jerperez@paul-f4Br1s1.clusters.42paris.fr>
2025-02-20 09:02:00 +0100

    try cath removed trash

--- cli/pong-cli.js
+++ cli/pong-cli.js
@@ -5,13 +5,22 @@ const {CmdGame} = require("./CmdGame");
 const {CmdTest} = require("./CmdTest");
 const {HttpsClient} = require("./HttpsClient");
 
+function main () {
+    const p = new Parser();
+    patterns = ["[--help]", "[--version]"]
+    callbacks = [()=>{p.displayHelp = true}, ()=>{process.stdout.write("pong-cli version 0.1.0\n")}]
+    p.setOptions(patterns, callbacks);
+    p.commands = [new CmdRegister(), new CmdChat(), new CmdGame(), new CmdTest()];
+    p.defaultCallback = ()=>{p.printHelp()};
+    
+    p.eval();
+}
+
 HttpsClient.allowSelfSigned(); //WARNING: ALLOWS SELF SIGNED (OK for eval)
 
-const p = new Parser();
-patterns = ["[--help]", "[--version]"]
-callbacks = [()=>{p.displayHelp = true}, ()=>{process.stdout.write("pong-cli version 0.1.0\n")}]
-p.setOptions(patterns, callbacks);
-p.commands = [new CmdRegister(), new CmdChat(), new CmdGame(), new CmdTest()];
-p.defaultCallback = ()=>{p.printHelp()};
-
-p.eval();
+try {
+    main();
+} catch (e) {
+    console.log(`pong-cli: exited with code 1: ${e}`);
+    process.exit(1);
+}
